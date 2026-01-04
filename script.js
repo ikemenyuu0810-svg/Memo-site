bcb2d823eb0ce0b00b1b528a6e47d992a4dde7e1
fukufukuyuu <ikemenyuu0810@gmail.com>
2026-01-04 19:34:09 +0900

    a

--- script.js
+++ script.js
@@ -245,9 +245,9 @@
         // 簡易Markdownパーサー
         function parseMarkdown(text) {
             return text
-                .replace(/^#(.*$)/gm, '<h1>$1</h1>')
-                .replace(/^##(.*$)/gm, '<h2>$1</h2>')
-                .replace(/^###(.*$)/gm, '<h3>$1</h3>')
+                .replace(/^# (.*$)/gm, '<h1>$1</h1>')
+                .replace(/^## (.*$)/gm, '<h2>$1</h2>')
+                .replace(/^### (.*$)/gm, '<h3>$1</h3>')
                 .replace(/\*\*(.*?)\*\*/g, '<strong>$1</strong>')
                 .replace(/\*(.*?)\*/g, '<em>$1</em>')
                 .replace(/`(.*?)`/g, '<code>$1</code>')
